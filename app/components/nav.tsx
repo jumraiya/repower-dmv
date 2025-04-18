8fe8854f606979657ef94a718a041f930e685fd8
Wendy H. Chun <wendy.hj.chun@gmail.com>
2025-04-18 19:08:32 -0400

    Add navbar for the application page (#66)
    - Add "Apply" to navbar.
    - Move other navbar items to vertical center.
    
    I added "Apply" as a button, inspired by this page:
    https://www.volunteerfairfax.org/programs/
    I can change the color and styling if this green doesn't match with the
    bg :)
    
    
    ![image](https://github.com/user-attachments/assets/d571fe65-2795-49b0-b78e-1ae1ffcb17ba)

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,5 +1,5 @@
-import { NavLink } from '@remix-run/react';
-import { useState } from 'react';
+import { NavLink } from "@remix-run/react";
+import { useState } from "react";
 
 function Navbar() {
   const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
@@ -9,57 +9,81 @@ function Navbar() {
   };
 
   return (
-    <nav className="fixed top-0 left-0 right-0 py-4 px-6 text-white bg-repower-dark-blue z-50">
-      <div className="flex flex-wrap md:flex-nowrap justify-items-center items-center">
+    <nav className="fixed left-0 right-0 top-0 z-50 bg-repower-dark-blue px-6 py-4 text-white">
+      <div className="flex flex-wrap items-center justify-items-center md:flex-nowrap">
         <NavLink to="/">
-            <img src="img/electrifydmvwhitehorizontal.png" alt="Electrify DMV logo" className="inline-block h-fit w-[250px] md:w-[300px] lg:w-[350px] md:mr-10 grow md:grow-0" />
+          <img
+            src="img/electrifydmvwhitehorizontal.png"
+            alt="Electrify DMV logo"
+            className="inline-block h-fit w-[250px] grow md:mr-10 md:w-[300px] md:grow-0 lg:w-[350px]"
+          />
         </NavLink>
-        <div className="min-w-4 grow">
+        <div className="min-w-4 grow"></div>
+        <div
+          className="cursor-pointer md:hidden"
+          onClick={toggleMobileMenu}
+          onKeyDown={toggleMobileMenu}
+          role="presentation"
+        >
+          <i
+            className={`fa fa-2x aria-hidden='true' ${isMobileMenuToggled ? "fa-times" : "fa-bars"}`}
+          ></i>
         </div>
-        <div className="cursor-pointer md:hidden" onClick={ toggleMobileMenu } onKeyDown={ toggleMobileMenu } role="presentation"><i className={`fa fa-2x aria-hidden='true' ${isMobileMenuToggled ? 'fa-times' : 'fa-bars'}`}></i></div>
-        <div className={`md:inline-block grow md:grow-0 basis-full md:basis-auto mt-4 md:mt-0 ${isMobileMenuToggled ? 'inline-block' : 'hidden'}`} onClick={ () => setIsMobileMenuToggled(false) } onKeyDown={ () => setIsMobileMenuToggled(false) } role="presentation">
-            <ul className="flex flex-col md:flex-row justify-end text-center text-nowrap text-lg [&>li]:inline-block gap-y-4 gap-x-8 [&>li:hover]:text-gray-300">
-                <li className="inline-block">
-                <NavLink
-                    to="/about"
-                    className={({ isActive, isPending }) =>
-                    isPending ? "border-b-4" : isActive ? "border-b-4" : ""
-                    }
-                >
-                    About
-                </NavLink>
-                </li>
-                <li className="inline-block">
-                <NavLink
-                    to="/contractors"
-                    className={({ isActive, isPending }) =>
-                    isPending ? "border-b-4" : isActive ? "border-b-4" : ""
-                    }
-                >
-                    Contractor List
-                </NavLink>
-                </li>
-                <li className="inline-block">
-                <NavLink
-                    to="/incentives"
-                    className={({ isActive, isPending }) =>
-                    isPending ? "border-b-4" : isActive ? "border-b-4" : ""
-                    }
-                >
-                    Incentives
-                </NavLink>
-                </li>
-                <li className="inline-block">
-                <NavLink
-                    to="/resources"
-                    className={({ isActive, isPending }) =>
-                    isPending ? "border-b-4" : isActive ? "border-b-4" : ""
-                    }
-                >
-                    Resources
-                </NavLink>
-                </li>
-            </ul>
+        <div
+          className={`mt-4 grow basis-full md:mt-0 md:inline-block md:grow-0 md:basis-auto ${isMobileMenuToggled ? "inline-block" : "hidden"}`}
+          onClick={() => setIsMobileMenuToggled(false)}
+          onKeyDown={() => setIsMobileMenuToggled(false)}
+          role="presentation"
+        >
+          <ul className="flex flex-col items-center justify-end gap-x-8 gap-y-4 text-nowrap text-center text-lg md:flex-row [&>li:hover]:text-gray-300 [&>li]:inline-block">
+            <li className="inline-block">
+              <NavLink
+                to="/about"
+                className={({ isActive, isPending }) =>
+                  isPending ? "border-b-4" : isActive ? "border-b-4" : ""
+                }
+              >
+                About
+              </NavLink>
+            </li>
+            <li className="inline-block">
+              <NavLink
+                to="/contractors"
+                className={({ isActive, isPending }) =>
+                  isPending ? "border-b-4" : isActive ? "border-b-4" : ""
+                }
+              >
+                Contractor List
+              </NavLink>
+            </li>
+            <li className="inline-block">
+              <NavLink
+                to="/incentives"
+                className={({ isActive, isPending }) =>
+                  isPending ? "border-b-4" : isActive ? "border-b-4" : ""
+                }
+              >
+                Incentives
+              </NavLink>
+            </li>
+            <li className="inline-block">
+              <NavLink
+                to="/resources"
+                className={({ isActive, isPending }) =>
+                  isPending ? "border-b-4" : isActive ? "border-b-4" : ""
+                }
+              >
+                Resources
+              </NavLink>
+            </li>
+            <li className="align-middle">
+              <NavLink to="/apply">
+                <button className="rounded bg-green-700 px-6 py-2 hover:bg-green-800">
+                  Apply
+                </button>
+              </NavLink>
+            </li>
+          </ul>
         </div>
       </div>
     </nav>
